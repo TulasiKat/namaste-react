df25b9bf4d7c7041bb299ae4541c8f10b0871c30
Tulasi Katukoju <tulasikatukoju@Tulasis-MacBook-Pro.local>
2024-07-10 16:14:07 +0530

    episode3 - functional components- start

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-const heading = React.createElement("h1" , {id:"heading"}, "hello world from react!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
 
+const Heading = () => {
+  return <>
+  <h1>hello h1</h1>
+ <Para></Para>
+  </>;      
+};
 
-const parent = React.createElement("div" , {id:"parent"} , 
-React.createElement("div"  , {id:"child"} , [
-    React.createElement("h1" , {id:"heading"} , "hello heading!") , 
-    React.createElement("p" , {id:"para"} , "hello  para!")
-])
+const Para = () => (
+<p>hello p</p>
 );
+     
 
 
-root.render(parent);
+
+root.render(<Heading/>);
