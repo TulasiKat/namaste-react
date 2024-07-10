b5db193bcc729db9effb3ab2713513e470036847
Tulasi Katukoju <tulasikatukoju@Tulasis-MacBook-Pro.local>
2024-07-10 14:20:39 +0530

    added parcel and run build

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 const heading = React.createElement("h1" , {id:"heading"}, "hello world from react!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
@@ -5,14 +9,10 @@ root.render(heading);
 
 const parent = React.createElement("div" , {id:"parent"} , 
 React.createElement("div"  , {id:"child"} , [
-    React.createElement("h1" , {id:"heading"} , "hello world heading!") , 
-    React.createElement("p" , {id:"para"} , "hello world para!")
+    React.createElement("h1" , {id:"heading"} , "hello heading!") , 
+    React.createElement("p" , {id:"para"} , "hello  para!")
 ])
 );
 
 
 root.render(parent);
-
-
-const root2 = ReactDOM.createRoot(document.getElementById("root2"));
-root2.render(parent);
